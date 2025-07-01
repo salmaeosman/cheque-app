e229dd55392ffcbbe2aa1c0436ec60523d83b135
Salmae <salma2233447@gmail.com>
2025-07-01 09:34:25 +0100

    Initial commit - Spring Boot Cheque Project

---
+++ src/main/java/bp/projetbanque/GestionCheque/App.java
@@ -0,0 +1,10 @@
+package bp.projetbanque.GestionCheque;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
